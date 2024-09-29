a211f7d76f89786577da16fe8bad4d6b756994ed
egorycheva28 <egor.masha28@list.ru>
2024-09-29 14:20:06 +0700

    добавлен get-запрос (получение списка), добавлен post-запрос (добавление новой задачи)

--- js.js
+++ js.js
@@ -1,8 +1,144 @@
 let todoList = document.getElementById('todoList');
 const todoInput = document.getElementById('todoInput');
 const addButton = document.getElementById('addButton');
+const url = 'http://localhost:5186/api/todo'; // URL для запроса
+let list = [];
+
+// Выполнение GET-запроса
+fetch(url)
+    .then(response => {
+        // Проверка, успешен ли ответ
+        if (!response.ok) {
+            throw new Error('Ошибка сети');
+        }
+        return response.json(); // Преобразуем ответ в формат JSON
+    })
+    .then(data => {
+        list = data;
+        //console.log(data); // Обработка данных
+        data.forEach(task => {
+            getTask(task);
+        });
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
 
 function addTask() {
+    if (todoInput.value === '') {
+        alert("Введите дело");
+    }
+    else {
+        let task = todoInput.value;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(task)
+        })
+        .then(response => {
+            // Проверка, успешен ли ответ
+            if (!response.ok) {
+                throw new Error('Ошибка сети');
+            }
+            //return response.json(); // Преобразуем ответ в формат JSON
+        })
+        .catch (error=> {
+            console.error('Ошибка:', error);
+            alert('Не удалось добавить задачу. Попробуйте снова.');
+        });
+    }
+    todoInput.value = "";
+    getTasks();
+}
+
+function getTasks() {
+    fetch(url)
+        .then(response => {
+            // Проверка, успешен ли ответ
+            if (!response.ok) {
+                throw new Error('Ошибка сети');
+            }
+            return response.json(); // Преобразуем ответ в формат JSON
+        })
+        .then(data => {
+            list = data;
+            console.log(data); // Обработка данных
+            todoList.innerHTML = "";
+            data.forEach(task => {
+                getTask(task);
+            });
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+}
+
+function getTask(task) {
+    let li = document.createElement('li');
+    li.innerHTML = task.description;
+
+    let button = document.createElement('button');
+    button.className = 'edit';
+    let img = document.createElement('img');
+    img.src = 'edit.png';
+    img.className = 'Edit';
+    button.appendChild(img);
+    li.appendChild(button);
+
+    let span = document.createElement('span');
+    span.innerHTML = '\u00d7';
+    li.appendChild(span);
+
+    todoList.appendChild(li);
+}
+
+/*todoList.addEventListener('click', function (event) {
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('check');
+    }
+    else if (event.target.tagName === 'SPAN') {
+        //delete запрос
+        let idd = event.target.parentElement.textContent;
+        let taskId = 0;
+        for (let i of list) {
+            if (i.description + '\u00d7' == idd) {
+                taskId = i.id;
+                break;
+            }
+            console.log(i.description);
+        }
+
+        console.log(taskId);
+        fetch(`http://localhost:5186/api/todo/${taskId}`, {
+            method: 'DELETE' // Указываем метод DELETE
+        })
+            .then(response => {
+                if (response.ok) {
+                    console.log('Ресурс успешно удален'); // Успешное удаление
+                } else {
+                    console.error('Ошибка при удалении ресурса:', response.statusText); // Ошибка удаления
+                }
+            })
+            .catch(error => {
+                console.error('Произошла ошибка:', error); // Ошибка сети или другие ошибки
+            });
+        getTasks();
+    }
+    else if (event.target.className === 'Edit') {
+        let currentLi = event.target.parentElement.parentElement;
+        let currentText = currentLi.firstChild.textContent;
+        let editedText = prompt("Редактировать дело:", currentText);
+        if (editedText !== null && editedText !== '') {
+            //put запрос
+            //currentLi.firstChild.textContent = editedText;
+        }
+
+    }
+}, false);*/
+
+/*function addTask() {
     if (todoInput.value === '') {
         alert("Введите дело");
     }
@@ -97,7 +233,7 @@ openFile.addEventListener('change', function () {
     };
 
     reader.readAsText(openFile.files[0]);
-});
+});*/
 
 
 
